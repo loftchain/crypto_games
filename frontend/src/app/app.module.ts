063694c6e714db4e518544c041c39a1f9142bb1f
Sergej <serezha11shumov@mail.ru>
2019-01-10 16:04:21 +0300

    adding footer component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from './app.component';
-
-import { HeaderComponent } from './navigation/header/header.component';
-import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import {MaterialModule} from "./material.module";
+import { MaterialModule } from "./material.module";
 
 // import ngx-translate and the http loader
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+
+import {HeaderComponent} from "./navigation/header/header.component";
+import {SidenavListComponent} from "./navigation/sidenav-list/sidenav-list.component";
+import { FooterComponent } from './footer/footer.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
