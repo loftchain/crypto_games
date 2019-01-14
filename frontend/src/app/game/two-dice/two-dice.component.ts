ab537299796f1a81a528ed40f88339ba6a08fbfb
Sergej <serezha11shumov@mail.ru>
2019-01-14 18:04:44 +0300

    improve two-dice functionality

--- frontend/src/app/game/two-dice/two-dice.component.ts
+++ frontend/src/app/game/two-dice/two-dice.component.ts
@@ -8,19 +8,19 @@ import {GameService} from "../game.service";
 })
 export class TwoDiceComponent implements OnInit {
   winChance = 2.78;
-  // chanceTable = {
-  //   '2': 1,
-  //   '3': 2,
-  //   '4': 3,
-  //   '5': 4,
-  //   '6': 5,
-  //   '7': 6,
-  //   '8': 5,
-  //   '9': 4,
-  //   '10': 3,
-  //   '11': 2,
-  //   '12': 1
-  // };
+  chanceTable = {
+    '2': 1,
+    '3': 2,
+    '4': 3,
+    '5': 4,
+    '6': 5,
+    '7': 6,
+    '8': 5,
+    '9': 4,
+    '10': 3,
+    '11': 2,
+    '12': 1
+  };
 
   constructor(private gameService: GameService) { }
 
@@ -49,30 +49,15 @@ export class TwoDiceComponent implements OnInit {
     const activeDices = document.body.querySelectorAll('.two-dice .dice-item.active');
     const self = this;
     let sum = 0;
-    const chanceTable = {
-      '2': 1,
-      '3': 2,
-      '4': 3,
-      '5': 4,
-      '6': 5,
-      '7': 6,
-      '8': 5,
-      '9': 4,
-      '10': 3,
-      '11': 2,
-      '12': 1
-    };
     activeDices.forEach(function(item) {
-      const divider = 36;
       const num = item.textContent;
-      sum += +((chanceTable[num] / divider) * 100).toFixed(2);
+      sum += self.countWinChance(num);
     });
     this.winChance = sum;
-    console.log(this.winChance);
   }
 
   countWinChance(num) {
     const divider = 36;
-    return ((num / divider) * 100).toFixed(2);
+    return +((this.chanceTable[num] / divider) * 100).toFixed(2);
   }
 }
